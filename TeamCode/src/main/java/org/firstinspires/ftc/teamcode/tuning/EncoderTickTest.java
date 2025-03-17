290055c8da87381eea137599ce2f53a9ac8f33df
GTortoise07 <29tuw@smtexas.org>
2025-03-16 19:32:03 -0500

    3-14-25

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/EncoderTickTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/EncoderTickTest.java
@@ -7,16 +7,39 @@ import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.Drawing;
 
-
 import org.firstinspires.ftc.teamcode.TankDrive;
 public class EncoderTickTest extends LinearOpMode{
-    public void runOpMode() {
+    @Override
+    public void runOpMode() throws InterruptedException{
+        DcMotorEx leftMotor = hardwareMap.get(DcMotorEx.class, "leftFront");
+        DcMotorEx rightMotor = hardwareMap.get(DcMotorEx.class, "rightFront");
 
+        leftMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftMotor.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotor.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            float leftPosition = leftMotor.getCurrentPosition();
+            float rightPosition = rightMotor.getCurrentPosition();
+            float totalPosition = leftPosition + rightPosition;
+            /*
+            telemetry.addData("Left Encoder", leftPosition);
+            telemetry.addData("Right Encoder", rightPosition);
+             */
+            telemetry.addData("Encoder:", totalPosition);
+            telemetry.update();
+        }
 
 
     }
+
 
 }
