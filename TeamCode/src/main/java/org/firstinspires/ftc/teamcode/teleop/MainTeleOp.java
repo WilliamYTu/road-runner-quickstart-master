5f2bf6623752d3ff8b71e7c2bfc531459964125a
GTortoise07 <29tuw@smtexas.org>
2025-03-28 20:28:56 -0500

    3-28-25

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
@@ -0,0 +1,89 @@
+package org.firstinspires.ftc.teamcode.teleop;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+//import com.acmerobotics.dashboard.FtcDashboard;
+//import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+
+import org.firstinspires.ftc.teamcode.teleop.ProgrammingBoard4;
+
+@TeleOp()
+public class MainTeleOp extends OpMode{
+    ProgrammingBoard4 board = new ProgrammingBoard4();
+    //FtcDashboard dashboard = FtcDashboard.getInstance();
+    //TelemetryPacket packet = new TelemetryPacket();
+
+    @Override
+    public void init(){
+        board.init(hardwareMap);
+
+    }
+    @Override
+    public void loop(){
+        // dashboard.sendTelemetryPacket(packet);
+        double speedForward = -gamepad1.left_stick_y / 2.0;
+        double turnSpeed = -gamepad1.right_stick_x / 2.0;
+
+
+        if (gamepad1.b) {
+            board.moveArm(1);
+        } else if (gamepad1.a) {
+            board.moveArm(-1);
+        } else {
+            board.moveArm(0);
+        }
+
+
+        if (gamepad1.y){
+            board.moveIntake(1);
+        } else if (gamepad1.x){
+            board.moveIntake(-1);
+        } else {
+            board.moveIntake(0);
+        }
+
+        // Intake
+        if (gamepad1.dpad_up){
+            board.moveIntakeServo(2);
+        } else if (gamepad1.dpad_down) {
+            board.moveIntakeServo(-2);
+        } else {
+            board.moveIntakeServo(0);
+        }
+        // Claw
+        if (gamepad1.dpad_right){
+            board.moveClawServo(0.5);
+        } else{
+            board.moveClawServo(1);
+        }
+
+
+        if (gamepad1.left_stick_y >= 0.2 || gamepad1.left_stick_y <= -0.2){
+            board.setMotorSpeed(-speedForward);
+        } else {
+            if (gamepad1.right_stick_x > 0 || gamepad1.right_stick_x < 0) {
+                board.turnRobot(-turnSpeed);
+            }
+            else {
+                board.setMotorSpeed(0);
+            }
+        }
+        telemetry.addData("Intake Motor:", board.getMotorPosition("intakeMotor"));
+
+        telemetry.addData("Arm Motor:", board.getMotorRotations("armMotor"));
+        telemetry.addData("Left stick x", gamepad1.left_stick_x);
+        telemetry.addData("Left stick y", gamepad1.left_stick_y);
+        telemetry.addData("Right stick x", gamepad1.right_stick_x);
+        telemetry.addData("Right stick y", gamepad1.right_stick_y);
+        telemetry.addData("A button", gamepad1.a);
+        telemetry.addData("B button", gamepad1.b);
+        /*
+        packet.put("Left stick x", gamepad1.left_stick_x);
+        packet.put("Left stick y", gamepad1.left_stick_y);
+        packet.put("Right stick x", gamepad1.right_stick_x);
+        packet.put("Right stick y", gamepad1.right_stick_y);
+        packet.put("A button", gamepad1.a);
+        packet.put("B button", gamepad1.b);
+        */
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ProgrammingBoard4.java
@@ -0,0 +1,109 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ProgrammingBoard4 {
+    private DigitalChannel touchSensor;
+    private DcMotor motor1;
+    private DcMotor motor2;
+    private DcMotor armMotor;
+    private DcMotor intakeMotor;
+    private CRServo intakeServo;
+    private Servo clawServo;
+    private double ticksPerRotation;
+
+
+    public void init(HardwareMap hwMap){
+        motor1 = hwMap.get(DcMotor.class, "motor");
+        motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor2 = hwMap.get(DcMotor.class, "motor2");
+        motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotor = hwMap.get(DcMotor.class, "armMotor");
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intakeMotor = hwMap.get(DcMotor.class, "intakeMotor");
+        intakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intakeServo = hwMap.get(CRServo.class, "intakeServo");
+        clawServo = hwMap.get(Servo.class, "clawServo");
+        clawServo.scaleRange(0.5, 1);
+        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        ticksPerRotation = motor1.getMotorType().getTicksPerRev();
+    }
+    public boolean isTouchSensorPressed(){
+        return !touchSensor.getState();
+    }
+    public int getMotorPosition(String motorName){
+        if (motorName == "motor1"){
+            return motor1.getCurrentPosition();
+        } else if (motorName == "motor2") {
+            return motor2.getCurrentPosition();
+        } else if (motorName == "armMotor"){
+            return armMotor.getCurrentPosition();
+        } else if (motorName == "intakeMotor"){
+            return intakeMotor.getCurrentPosition();
+        } else {
+            return 0;
+        }
+    }
+    public void setMotorSpeed(double speed){
+        motor1.setPower(speed);
+        motor2.setPower(-speed);
+    }
+    public void moveIntakeServo(float direction){
+        intakeServo.setPower(direction / 2);
+    }
+    public void moveClawServo(double position){
+        clawServo.setPosition(position);
+    }
+    public void moveClaw(double position){
+        clawServo.setPosition(position);
+    }
+    public void moveArm(double direction){
+        armMotor.setPower(direction / 2);
+    }
+    public void turnRobot(double speed){
+        motor1.setPower(speed);
+        motor2.setPower(speed);
+    }
+    public void moveIntake(float direction){
+        intakeMotor.setPower(direction / 2);
+    }
+    public void resetEncoders(){
+        motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        intakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+    }
+    public double getMotorRotations(String motorName) {
+        if (motorName == "motor1"){
+            return motor1.getCurrentPosition();
+        } else if (motorName == "motor2") {
+            return motor2.getCurrentPosition();
+        } else if (motorName == "armMotor"){
+            return armMotor.getCurrentPosition();
+        } else if (motorName == "intakeMotor"){
+            return intakeMotor.getCurrentPosition();
+        } else {
+            return 0;
+        }
+    }
+    public void setZeroPowerBehavior(boolean gamepad1a, boolean gamepad1b){
+        if (gamepad1a == true){
+            motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+        if (gamepad1b == true){
+            motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        }
+    }
+}
